ac33e09acca0b4321485f4e4d1c85da6fb98648a
hanamiya <ochuyanga8@gmail.com>
2025-04-30 19:44:00 +0800

    first commit

---
+++ src/layouts/main-layout/topbar/index.tsx
@@ -0,0 +1,95 @@
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Badge from '@mui/material/Badge';
+import Toolbar from '@mui/material/Toolbar';
+import TextField from '@mui/material/TextField';
+import ButtonBase from '@mui/material/ButtonBase';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Box from '@mui/material/Box';
+import IconifyIcon from 'components/base/IconifyIcon';
+import Image from 'components/base/Image';
+import LogoImg from 'assets/images/Logo.png';
+import LanguageSelect from './LanguageSelect';
+import ProfileMenu from './ProfileMenu';
+
+interface TopbarProps {
+  isClosing: boolean;
+  mobileOpen: boolean;
+  setMobileOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Topbar = ({ isClosing, mobileOpen, setMobileOpen }: TopbarProps) => {
+  const handleDrawerToggle = () => {
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
+
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+      px={3.5}
+      height={90}
+      bgcolor="info.lighter"
+      position="sticky"
+      top={0}
+      zIndex={1200}
+    >
+      {/* Left Section */}
+      <Box display="flex" alignItems="center" minWidth="150px">
+        <ButtonBase
+          component={Link}
+          href="/"
+          disableRipple
+          sx={{ lineHeight: 0, display: { xs: 'none', sm: 'block', lg: 'none' } }}
+        >
+          <Image src={LogoImg} alt="logo" height={54} width={54} />
+        </ButtonBase>
+
+        <Toolbar sx={{ display: { xs: 'block', lg: 'none' }, ml: 1 }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerToggle}
+          >
+            <IconifyIcon icon="clarity:menu-line" />
+          </IconButton>
+        </Toolbar>
+      </Box>
+
+      {/* Center Section (Search) */}
+      <Box flex={1} display="flex" justifyContent="center">
+        <TextField
+          variant="filled"
+          placeholder="Search Task"
+          sx={{ width: 350, display: { xs: 'none', md: 'flex' } }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconifyIcon icon={'mynaui:search'} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      {/* Right Section */}
+      <Box display="flex" alignItems="center" gap={2}>
+        <LanguageSelect />
+        <IconButton size="large">
+          <Badge color="error" variant="dot">
+            <IconifyIcon icon="solar:bell-outline" />
+          </Badge>
+        </IconButton>
+        <ProfileMenu />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Topbar;
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,83 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Activity from 'components/sections/dashboard/activity';
+import TaskToday from 'components/sections/dashboard/task-today';
+import RunningTask from 'components/sections/dashboard/running-task';
+import UpcomingTask from 'components/sections/dashboard/upcoming-task';
+import WeekCalendar from 'components/sections/dashboard/week-calendar';
+import TaskOverview from 'components/sections/dashboard/task-overview';
+import MonthlyMentors from 'components/sections/dashboard/monthly-mentors';
+import Footer from 'components/common/Footer';
+
+const Dashboard = () => {
+  return (
+    <Box sx={{ width: '80vw', minHeight: '100vh', overflowX: 'hidden' }}>
+    <Stack
+      direction={{ xs: 'column', md: 'row' }}
+      sx={{
+        width: '100%',
+      }}
+    >
+      {/* Main Section */}
+      <Stack
+        p={3.5}
+        spacing={3.5}
+        direction="column"
+        width={{ xs: '100%', md: '100%' }}
+        boxSizing="border-box"
+      >
+        <Stack
+          width="100%"
+          spacing={3.5}
+          direction={{ xs: 'column', sm: 'row', md: 'column', xl: 'row' }}
+          flexWrap="wrap"
+        >
+          <RunningTask />
+          <Activity />
+        </Stack>
+
+        <MonthlyMentors />
+        <UpcomingTask />
+        <TaskOverview />
+
+        {/* Desktop Footer */}
+        <Box display={{ xs: 'none', md: 'block' }}>
+          <Footer />
+        </Box>
+      </Stack>
+
+      {/* Sidebar */}
+      <Box
+        width={{ xs: '100%', md: 460 }}
+        height={{ xs: 'auto', md: 'calc(100dvh - 90px)' }}
+        overflow="auto"
+        bgcolor="info.main"
+        position={{ xs: 'relative', md: 'sticky' }}
+        top={90}
+        sx={{
+          '&::-webkit-scrollbar-thumb': {
+            visibility: 'hidden',
+          },
+          '&:hover, &:focus': {
+            '&::-webkit-scrollbar-thumb': {
+              visibility: 'visible',
+            },
+          },
+        }}
+      >
+        <Stack p={3.5} spacing={3.5} width="100%" direction="column">
+          <WeekCalendar />
+          <TaskToday />
+        </Stack>
+
+        {/* Mobile Footer */}
+        <Box display={{ xs: 'block', md: 'none' }}>
+          <Footer />
+        </Box>
+      </Box>
+    </Stack>
+    </Box>
+  );
+};
+
+export default Dashboard;
